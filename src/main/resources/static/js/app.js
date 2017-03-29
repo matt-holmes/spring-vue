72026e2a5ffa74b834a5d6420466fcd67c9aac5a
Matt Holmes <mattholmes@live.com>
2017-03-28 21:24:25 -0500

    adding more around login/logout

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,49 +1,37 @@
-(function(){
-    var cookies;
-
-    function readCookie(name,c,C,i){
-        if(cookies){ return cookies[name]; }
-
-        c = document.cookie.split('; ');
-        cookies = {};
-
-        for(i=c.length-1; i>=0; i--){
-           C = c[i].split('=');
-           cookies[C[0]] = C[1];
-        }
-
-        return cookies[name];
-    }
-
-    window.readCookie = readCookie; // or expose it however you want
-})();
 Vue.http.headers.common['X-XSRF-TOKEN'] = readCookie('XSRF-TOKEN');
 
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
         username: '',
         password: '',
+        isSignedIn: false
     },
     methods: {
         getGreeting: function(event) {
             var self = this;
-            var token = readCookie('XSRF-TOKEN');
             this.$http.get('/greeting',
                 {headers : {
                     authorization : "Basic "+ btoa(self.username + ":" + self.password)
                 }
             }).then(function(response){
                 if(response.data) {
+                    self.isSignedIn = true;
                     self.message = response.data.id;
-                } else {
-                    window.location.assign("http://localhost:8080/login.html");
+                    $('#loginModal').modal('toggle');
                 }
             }).catch(e => {
                 console.log(e);
-                window.location.assign("http://localhost:8080/login.html");
-            });;
+                alert('invalid password');
+            });
         },
+
+        logout: function() {
+            this.$http.post('/logout',{}).then(function(response){
+                self.isSignedIn = false;
+            }).catch(e => {
+                console.log(e);
+            });
+        }
     }
 })
